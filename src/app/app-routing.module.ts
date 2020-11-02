4c9ed9ec70db80b2cbb2920827ef853911052160
Arijeet Rakshit <49709000+ArijeetRakshit@users.noreply.github.com>
2020-11-02 22:05:23 +0530

    Added Angular Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './component/todo-list/todo-list.component';
+import { TodoDetailsComponent } from './component/todo-details/todo-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'tl',component:TodoListComponent },
+  { path:'td',component:TodoDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
